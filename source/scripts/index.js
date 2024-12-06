10d387a5e971ab08221a2749a4e9b2b422c71ec3
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2024-12-06 08:22:50 +0500

    Оживляет бургер кнопку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+const buttonWrapper = document.querySelector('.button-wrapper');
+const burgerButton = document.querySelector('.burger-button');
+
+buttonWrapper.addEventListener('click', () => {
+
+  burgerButton.classList.toggle('burger-button--show');
+
+});
