d55db7b634a5081db24d4e18203896f0237859c7
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2024-12-12 08:57:14 +0500

    Добавляет адаптив меню и хедеру

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,10 @@
 /* в этот файл добавляет скрипты*/
 const buttonWrapper = document.querySelector('.button-wrapper');
 const burgerButton = document.querySelector('.burger-button');
+const navigationList = document.querySelector('.navigation__list');
 
 buttonWrapper.addEventListener('click', () => {
 
   burgerButton.classList.toggle('burger-button--show');
-
+  navigationList.classList.toggle('navigation__list--show');
 });
