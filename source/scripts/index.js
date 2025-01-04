b563ee3320bb4efcff3155557b63c6d2902a7c37
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-01-04 20:23:28 +0500

    Добавляет стили

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,10 @@ const buttonWrapper = document.querySelector('.button-wrapper');
 const burgerButton = document.querySelector('.burger-button');
 const navigationList = document.querySelector('.navigation__list');
 
+if(navigationList.classList[1] === 'navigation__list--show') {
+  navigationList.classList.remove('navigation__list--show');
+}
+
 buttonWrapper.addEventListener('click', () => {
 
   burgerButton.classList.toggle('burger-button--show');
